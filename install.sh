648dec4653897471bfddbf77a1802600b85e90cc
XLB <xlb6324@gmail.com>
2025-04-15 19:57:53 +0800

    同步上游最新版

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ check_glibc_version() {
     
     required_version="2.30"
     if [[ "$(printf '%s\n' "$required_version" "$glibc_version" | sort -V | head -n1)" != "$required_version" ]]; then
-        echo -e "${red}GLIBC version $glibc_version is too old! Required: 2.32 or higher${plain}"
+        echo -e "${red}GLIBC version $glibc_version is too old! Required: 2.30 or higher${plain}"
         echo "Please upgrade to a newer version of your operating system to get a higher GLIBC version."
         exit 1
     fi
